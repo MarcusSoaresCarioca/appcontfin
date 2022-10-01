4bf59db00bac1987f2c3fa0dcf7803cf26202868
MarcusSoaresCarioca <111324245+MarcusSoaresCarioca@users.noreply.github.com>
2022-09-30 22:20:31 -0300

    mudando style

--- src/paginas/Home/index.js
+++ src/paginas/Home/index.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./style";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, FlatList } from "react-native";
 import { Feather } from '@expo/vector-icons';
 
 export default function Home() {
+
+    const [receitas, setReceitas] = useState(null);
+    const [despesas, setDespesas] = useState(null);
+
+
     return (
         <View style={styles.container}>
-           <View style={styles.divForm}>
+            
+            <View style={styles.divForm}>
                 <View style={styles.divInput1}>
                     <TextInput style={styles.txtinput}
                         placeholder="Receitas"
@@ -25,15 +31,23 @@ export default function Home() {
                 <TouchableOpacity style={styles.divIcon}>
                     <Feather name="navigation" size={50} color="#fff" />
                 </TouchableOpacity>
-                
+
+              
 
                 <View style={styles.divFinanceiro}>
                     <Text style={styles.txtfinanceiro}>Controle Financeiro</Text>
                 </View>
-            </View>
+                
+           
+                </View>
+
+                
+                
+            
+
 
             <View style={styles.divResult}>
-
+                
             </View>
         </View>
     )
